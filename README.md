c945e3ef7c4c3638ce6992404946b5f6520d95c0
leo.karoubi <leokaroubi@gmail.com>
2020-06-07 14:47:49 +0200

    init with hello world

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tetris
-vga tetris implementation
+# Kamet
+
+`Kamet` is a set of early kernel games.
+
+All the games are running with `VGA`. All the project is in `rust`.
+
+# Dependencies
+
+# Build
+
+# Run
+
+# Epilogue
+
+To learn rust language.
+
+Feel free to fork, use, improve.
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
+mod vga_buffer;
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    println!("Hello World{}", "!");
+
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }

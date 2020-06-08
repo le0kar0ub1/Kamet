13a01d3f9ecc1538e993de22cc5e9c321088976d
leo.karoubi <leokaroubi@gmail.com>
2020-06-08 20:50:45 +0200

    serial driver & qemu config

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use core::panic::PanicInfo;
 
 mod drivers;
 use drivers::vga_buffer;
+use drivers::serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    serial_println!("Hello World !",);
     loop {}
 }
 

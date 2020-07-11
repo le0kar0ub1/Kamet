8a5f8b7039c54a160a43f5a9d859bc5a89345d42
leo.karoubi <leokaroubi@gmail.com>
2020-07-11 19:16:24 +0200

    set/clear irq mask

--- src/kernel/pic.rs
+++ src/kernel/pic.rs
@@ -3,6 +3,7 @@ use pic8259_simple::ChainedPics;
 use lazy_static::lazy_static;
 use spin;
 use x86_64::structures::idt::InterruptStackFrame;
+use x86_64::instructions::port::Port;
 
 const INTERRUPT_INDEX_PIC1: u8 = 32;
 const INTERRUPT_INDEX_PIC2: u8 = 40;
@@ -14,6 +15,24 @@ pub fn irq_index(base: u8) -> u8 {
     base + 32
 }
 
+#[allow(dead_code)]
+pub fn set_irq_mask(irq: u8) {
+    let mut port: Port<u8> = Port::new(if irq < 8 { INTERRUPT_INDEX_PIC1 as u16 } else { INTERRUPT_INDEX_PIC2 as u16 } );
+    unsafe {
+        let value = port.read() | (1 << (if irq < 8 { irq } else { irq - 8 }));
+        port.write(value);
+    }
+}
+
+#[allow(dead_code)]
+pub fn clear_irq_mask(irq: u8) {
+    let mut port: Port<u8> = Port::new(if irq < 8 { INTERRUPT_INDEX_PIC1 as u16 } else { INTERRUPT_INDEX_PIC2 as u16 } );
+    unsafe {
+        let value = port.read() & !(1 << if irq < 8 { irq } else { irq - 8 });
+        port.write(value);
+    }
+}
+
 pub extern "x86-interrupt" fn timer_handler(_stackframe: &mut InterruptStackFrame) {
     unsafe {
         PICS.lock().notify_end_of_interrupt(32);
@@ -23,7 +42,6 @@ pub extern "x86-interrupt" fn timer_handler(_stackframe: &mut InterruptStackFram
 pub extern "x86-interrupt" fn keyboard_handler(_stack_frame: &mut InterruptStackFrame) {
     use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
     use spin::Mutex;
-    use x86_64::instructions::port::Port;
 
     lazy_static! {
         static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(
